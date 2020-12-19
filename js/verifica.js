994e0fdaebca57e723b8e1e0a49801a1072116c1
Nicodemo Zilli <nicozv13@gmail.com>
2020-12-18 23:47:45 -0600

    Actualizacion

--- js/verifica.js
+++ js/verifica.js
@@ -1,56 +1,58 @@
 document.write("<script type='text/javascript' src='js/ajax.js'></script>");
 document.write("<script src='https://cdn.jsdelivr.net/npm/sweetalert2@9'></script>");
 
-function alertmsj(tit,msj){
+function alertmsj(tit,msj,icon){
   var titulo=tit;
   var mensaje=msj;
   Swal.fire({
     title: titulo,
     text: mensaje,
+    icon: icon,
     allowOutsideClick: false,
     confirmButtonText: "Ok",
     confirmButtonColor: "#D4AC0D",
   });
 }
 
-
-
 let area = document.getElementById('area');
 
 document.getElementById("carga").addEventListener('change', leerArchivo, false);
 
 function leerArchivo(e) {
+  try{
   area.style.backgroundColor="#fff";
   area.textContent = '';
-  var archivo = e.target.files[0];
-  if (!archivo) {
-    alertmsj("Error!","No es un Archivo");
+  e.preventDefault();
+  let file = e.target.files[0];
+  if (!file) {
+    alertmsj("Error!","No es un Archivo",'warning');
     return;
   }
 
-  if (archivo.type === 'text/plain') {
-    var lector = new FileReader();
-    lector.onload = function(e) {
-      var contenido = e.target.result;
-      printFileContents(contenido);
-    };
-    lector.readAsText(archivo);
+  if (file.type === 'text/plain') {
+    var reader = new FileReader();
+    reader.onloadend = ()=>printFileContents(reader.result);
+    reader.readAsText(file,'ISO-8859-1');
   }else {
-    alertmsj("Error!","No es un archivo txt");
+    alertmsj("Error!","No es un archivo txt",'warning');
   }
+}catch(error){
+  alertmsj("Error","No se pudo cargar el archivo",'error');
+}
 }
 
 area.addEventListener('dragover', e => e.preventDefault());
 area.addEventListener('drop', readFile);
 
 function readFile (e) {
+  try{
   area.style.backgroundColor="#fff";
   area.textContent = '';
   e.preventDefault();
   let file = e.dataTransfer.files[0];
 
   if (!file) {
-    alertmsj("Error!","No es un Archivo");
+    alertmsj("Error!","No es un Archivo",'warning');
     return;
   }
 
@@ -59,7 +61,10 @@ function readFile (e) {
     reader.onloadend = () => printFileContents(reader.result);
     reader.readAsText(file, 'ISO-8859-1');
   } else {
-    alertmsj("Error!","No es un archivo txt");
+    alertmsj("Error!","No es un archivo txt",'warning');
+  }
+  }catch(error){
+    alertmsj("Error","No se pudo cargar el archivo",'error');
   }
 }
 
@@ -69,6 +74,7 @@ let jObject = null;
 
 
 function printFileContents (contents) {
+  try{
   cod=new Array();
   jObject={};
 
@@ -87,6 +93,9 @@ function printFileContents (contents) {
     {
         jObject[i] = cod[i];
     }
+  }catch(error){
+    alertmsj("Error","No se pudo mostrar el contenido",'error');
+  }
 }
 function encode_utf8(s) {
   return unescape(encodeURIComponent(s));
@@ -99,6 +108,7 @@ function decode_utf8(s) {
 document.getElementById('envia').addEventListener('click',getSyntax,false);
 
 function getSyntax(){
+  try{
   //console.log(cod);
   //console.log(jObject);
   //function getRequest(url, method, data, async, success, error, msg)
@@ -109,27 +119,37 @@ function getSyntax(){
         null,//{jObject: JSON.stringify(jObject)},
         true,
         AME,
-        drawError,
-        loadingmsg
+        ERR,
+        Loadingmsg
       );
   }else{
-    alertmsj("Error!","No se ha subido ningun archivo");
+    alertmsj("Error!","No se ha subido ningun archivo",'warning');
+  }
+  }catch(error){
+    alertmsj("Error","No se pudo mandar al servidor",'error');
   }
 }
+function ERR(rt){
+  alertmsj("ERROR REQUEST","No retorno nada",'error');
+}
 function AME(rt){
+try{
   var rtx = JSON.parse(rt);
-  alertmsj("Aviso",rtx[0]);
+  alertmsj("Felicidades",rtx[0],'success');
 
     if( rtx[0] == "Código Valido!!"){
       area.style.backgroundColor="rgba(0, 255, 0, 0.7)";
     }else{
       area.style.backgroundColor="rgba(255,0, 0, 0.7)";
     }
-  document.getElementById('output').innerHTML=rtx[1];
+    document.getElementById('output').innerHTML=rtx[1];
+}catch(error){
+  alertmsj("Error","El servidor no respondio",'error');
+}
 
 }
 
-function loadingmsg(){
+function Loadingmsg(){
   Swal.fire({
   title: 'Cargando...',
   showConfirmButton: false,
